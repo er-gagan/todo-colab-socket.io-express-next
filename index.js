247f756b8c966b22ef865ca0aad291c72a26ebd6
Gagan Aggarwal <113084015+gaganeducase@users.noreply.github.com>
2024-11-14 00:50:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 app.use('/api', routes);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! tested');
 });
 
 // Database connection and server start
